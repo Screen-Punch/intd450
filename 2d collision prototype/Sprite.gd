37e970c112511575161f75de2305744c5177be16
wdong2 <wdong2@ualberta.ca>
2019-01-26 19:20:47 -0700

    room transfer

---
+++ 2d collision prototype/Sprite.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+# class member variables go here, for example:
+export (String, FILE, "*.tscn") var next_world
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	var coli_list = get_overlapping_bodies()
+	for coli in coli_list:
+		if coli.name == "player":
+			get_tree().change_scene(next_world)
+	pass
+
+
