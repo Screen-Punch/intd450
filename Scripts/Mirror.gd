31297b99279b640aeaa56b7077ee88e2fc4d836e
Struckdown <bfleysher@gmail.com>
2019-02-06 19:58:59 -0700

    Added mirror mechanic

---
+++ Scripts/Mirror.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+func _ready():
+	pass
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		$Sprite.flip_v = true;
+		var enemies = get_tree().get_nodes_in_group("Monster")
+		for enemy in enemies:
+    		enemy.target = self
+
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("Player"):
+		$Sprite.flip_v = false;
+		var enemies = get_tree().get_nodes_in_group("Monster")
+		for enemy in enemies:
+			enemy.findNewTarget();
--- Scripts/Monster.gd
+++ Scripts/Monster.gd
@@ -5,10 +5,17 @@ var target
 var velocity
 
 func _ready():
-	target = get_tree().get_nodes_in_group("Player")[0]
+	var targets = get_tree().get_nodes_in_group("Player")
+	for t in targets:
+		target = t
 
 func _process(delta):
 	if target:
 		velocity = (target.position - position).normalized() * SPEED
 		if (target.position - position).length() > 5:
 			move_and_slide(velocity)
+
+func findNewTarget():
+	var targets = get_tree().get_nodes_in_group("Player")
+	for t in targets:
+		target = t	
