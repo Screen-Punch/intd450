f53bb0c8f7d65474bf2796e40b90c78c6cba6f59
Androcks <andrewwsom@gmail.com>
2019-02-08 18:21:21 -0700

    Added Sound File, as well as beginnings of sound scripting.

---
+++ Scripts/SoundManager.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer
+func _Protag_Sound(sfx, position):
+	
+	#sound location calculated relative to protagonist, redirected to main sound
+	#function used by other scripts involving diagetic sounds (switches, enemies, etc)
+	#volume determined by distance between source and PC
+	#Panning determined by direction.
+	#Volume reduction linear with Db
+	#as this is primarily for SFX, wav files are recommended.
+	
+	
+	
+func _BGM_Sound(sound):
+	
+	
+	
+	
