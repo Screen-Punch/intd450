05bcb3fc2d754d72e0c4f4ac0542bc1b61708431
Struckdown <bfleysher@gmail.com>
2019-02-17 15:49:44 -0700

    Mirrors now use LOS

--- Scripts/Mirror.gd
+++ Scripts/Mirror.gd
@@ -1,9 +1,23 @@
 extends StaticBody2D
 
+var RAYCASTING = false
+var target
+
 func _ready():
 	pass
 
+func _physics_process(delta):
+	if RAYCASTING:
+		var space_state = get_world_2d().direct_space_state
+		var result = space_state.intersect_ray(position, target.position, 
+											[self], collision_mask)
+		if result:
+			if result.collider.name == "Monster":
+				$Sprite.flip_v = true
+				result.collider.target = self
+
 func takeDamage():
+	RAYCASTING = false
 	var enemies = get_tree().get_nodes_in_group("Monster")
 	for enemy in enemies:
 		enemy.findNewTarget();
@@ -11,9 +25,9 @@ func takeDamage():
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Monster"):
-		$Sprite.flip_v = true;
-		body.target = self
+		RAYCASTING = true
+		target = body
 
 
 func _on_Area2D_body_exited(body):
-	pass
+	RAYCASTING = false
