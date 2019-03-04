ad8bc2541b80ab14938d33fbfeda37cb3303ae4e
guanfangdong <dong.guanfang.2014@gmail.com>
2019-03-03 20:37:25 -0700

    add maze level, monster stop by barrier

--- Scripts/Monster.gd
+++ Scripts/Monster.gd
@@ -93,6 +93,9 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 		canMove = true
 
 
+func change_speed(new_speed):
+	SPEED = new_speed
+
 func _on_Timer_timeout():
 	var index = randi() % len(randomMonsterNoises)
 	$AudioStreamPlayer2D.stream = load(randomMonsterNoises[index])
