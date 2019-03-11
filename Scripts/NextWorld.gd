57c44034cdbc01d77dcda95fcc054f963a1d6667
Struckdown <bfleysher@gmail.com>
2019-03-11 17:13:51 -0600

    Redid transition text for ease of access

--- Scripts/NextWorld.gd
+++ Scripts/NextWorld.gd
@@ -3,11 +3,31 @@ extends Area2D
 # class member variables go here, for example:
 export (String, FILE, "*.tscn") var next_world
 export (int) var level 
-export (String) var transitionText
 export (String) var nextLevelText
 var blocked = true
 var textCanAdvance = false
 var time = 0
+var transitionText
+var textTracker = 0
+var narrativeSequences = [
+["It was only a matter of time that another wanderlusting soul would find this place..."], #0 indexed
+["My wayward whispers… snaking and burrowing deeply into unsuspecting hearts and minds…",
+"Like a siren's song... how sweetly they promise the countless, irresistible desires and curiosities of the mortal heart..."],
+[""],
+["I-..."],
+["My solitary slumber was left unabated by guests for countless ages since the last intrusion...",
+"...you bear an uncanny resemblance to him. Interesting."],
+["The bittersweet stench of desperation reeks upon you… it betrays your every step.",
+"How you scurry about like a witless rat..."],
+["You may be able to run, and you may be able to buy yourself a moment's respite… but you will inevitably err."],
+[], # 7
+[],
+[],
+[], # 10
+[],
+[],
+[],
+]
 
 func _ready():
 	#level = 1
@@ -18,11 +38,9 @@ func _ready():
 	# Initialization here
 	if !next_world:
 		next_world = "res://Levels/Main Menu.tscn"
-	if !transitionText:
-		transitionText = "What is that sound..?"+str(time)
+	setTransitionText()
 	if !nextLevelText:
 		nextLevelText = "Level: ???"
-	$CanvasLayer/Label.text = transitionText
 
 
 func _process(delta):
@@ -31,20 +49,26 @@ func _process(delta):
 func _input(event):
 	if event is InputEventKey and textCanAdvance:
 		if event.pressed:
-			GameManagerNode.loadNextLevel(next_world, nextLevelText)
+			textCanAdvance = false
+			if textTracker < transitionText.size()-1:
+				textTracker += 1
+				$CanvasLayer/TextAnimator.play("TextTransition")
+				$CanvasLayer/Label.text = transitionText[textTracker]
+			else:
+				GameManagerNode.loadNextLevel(next_world, nextLevelText)
 
 func _on_next_W_body_entered(body):
 	if body.is_in_group("Player") and !blocked:
+		body.hide()
 		GameManagerNode.set_level_time()
 		time = GameManagerNode.get_level_time(level)
 		body.vulnerable = false
 		#_on_Timer_timeout() # Play Monster Noises while reading text
 		$CanvasLayer/AnimationPlayer.play("SceneTransition")
+		$CanvasLayer/TextAnimator.play("TextTransition")
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "SceneTransition":
-		textCanAdvance = true
 	if anim_name == "ExitUnlocked":
 		$StatueSprite.queue_free()
 
@@ -52,4 +76,18 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 func revealExit():
 	blocked = false
 	$CanvasLayer/AnimationPlayer.play("ExitUnlocked")
-	
+
+func setTransitionText():
+	var root = get_tree().root
+	var level
+	for child in root.get_children():
+		if "Level" in child.name:
+			level = child.name.split("-")[1]
+	level = int(level)
+	transitionText = narrativeSequences[level]
+	if transitionText.size() > 0:
+		$CanvasLayer/Label.text = transitionText[textTracker]
+
+func _on_TextAnimator_animation_finished(anim_name):
+	if anim_name == "TextTransition":
+		textCanAdvance = true
