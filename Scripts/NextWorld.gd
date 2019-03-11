6292de19067f84167d3df65cba424ce84cce4cc5
Struckdown <bfleysher@gmail.com>
2019-03-11 14:33:58 -0600

    Redid stair blocker animation

--- Scripts/NextWorld.gd
+++ Scripts/NextWorld.gd
@@ -44,6 +44,8 @@ func _on_next_W_body_entered(body):
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "SceneTransition":
 		textCanAdvance = true
+	if anim_name == "ExitUnlocked":
+		$StatueSprite.queue_free()
 
 
 func revealExit():
