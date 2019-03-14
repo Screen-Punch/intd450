292ff1033edc14a5a6cbfc47ed7aa146e4df2db4
Struckdown <bfleysher@gmail.com>
2019-03-14 13:15:29 -0600

    Levels on level selection are now hidden until unlocked

--- Scripts/Level1.gd
+++ Scripts/Level1.gd
@@ -5,6 +5,14 @@ extends Button
 # var b = "textvar"
 export (String, FILE, "*.tscn") var next_world
 
+func _ready():
+	var level
+	if "Level" in name:
+		level = name.split("Level")[1]
+		level = int(level)
+		if GameManagerNode.level_time[level] == -1:
+			hide()
+
 func _on_Level1_pressed():
 	GameManagerNode.loadNextLevel(next_world, " ")
 	GameManagerNode.set_level_selection_to_true()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -52,7 +52,7 @@ func _physics_process(delta):
 		motion += Vector2(-1, 0)
 	if Input.is_action_pressed("ui_right"):
 		motion += Vector2(1, 0)
-	if Input.is_action_pressed("ui_pause"):
+	if Input.is_action_pressed("ui_pause") and vulnerable:
 		$CanvasLayer/PauseMenu.show()
 		get_tree().paused = true
 	
