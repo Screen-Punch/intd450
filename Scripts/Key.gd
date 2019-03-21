cbe4842b9643263950c7c7336f2259052aa2cc91
wdong2 <wdong2@ualberta.ca>
2019-03-21 16:35:40 -0600

    debug, change intro level button

--- Scripts/Key.gd
+++ Scripts/Key.gd
@@ -12,7 +12,5 @@ func _on_Area2D_body_entered(body):
 		used = true
 		$AudioStreamPlayer2D.play(0)
 		emit_signal("keyGotten")
-		var enemies = get_tree().get_nodes_in_group("Monster")
-		for enemy in enemies:
-			enemy.findNewTarget();
+
 		$Sprite.frame = 1
