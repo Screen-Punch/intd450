1fbda73197c89ba77a766757b7a3601fa653278c
Struckdown <bfleysher@gmail.com>
2019-03-25 15:40:45 -0600

    Fixed broken level

--- Scripts/Key.gd
+++ Scripts/Key.gd
@@ -5,12 +5,12 @@ var monster
 var used = false
 
 func _ready():
-	pass
+	monster = get_tree().get_nodes_in_group("Monster")[0]
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player") and !used:
 		used = true
 		$AudioStreamPlayer2D.play(0)
 		emit_signal("keyGotten")
-
 		$Sprite.frame = 1
+		monster.findNewTarget()
