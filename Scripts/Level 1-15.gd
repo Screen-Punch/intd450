c75fc2e7855adc67ba7241fd011e27eef4742bad
Struckdown <bfleysher@gmail.com>
2019-04-06 13:33:15 -0600

    Fixed export files crashing

--- Scripts/Level 1-15.gd
+++ Scripts/Level 1-15.gd
@@ -7,13 +7,12 @@ func _ready():
 func _on_Player_spawned():
 	$Rubble.show()
 	$Timer.start(1)
-
+	$Player.canMove = false
 
 func _on_Timer_timeout():
 	name = "Level 1-0"
 	$CanvasLayer/TextTransition.setTransitionText()
 	$CanvasLayer/TextTransition/TextAnimator.play("ScreenBlockerFadeIn")
-	$Player.canMove = false
 
 
 func _on_TextTransition_textFadeOut():
